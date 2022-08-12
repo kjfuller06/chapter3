7c32398ff5f8eb6b782bf7e0fec3bd66ddfff05e
kjfuller06 <kjfuller06@gmail.com>
2022-08-12 15:30:38 -0500

    Updated with new files and methods- retained all severity == 0 shots for reference, stored in a different file; function stored in separate script

--- scripts/NCARjobs/ch3_extract_fesm_buffer_0.R
+++ scripts/NCARjobs/ch3_extract_fesm_buffer_0.R
@@ -5,142 +5,33 @@ library(tidyverse)
 library(geojsonsf)
 library(exactextractr)
 
-# extract fire dates ####
-setwd("/glade/scratch/kjfuller/data")
-veg = raster("veg_agg.tif")
+setwd("/glade/scratch/kjfuller/scripts/chapter3")
+source("extract_fesm_function.R")
 
-setwd("/glade/scratch/kjfuller/data/GEDI_fhist")
-gedi = st_read("g2_f3_fhist_12.5mbuffer.gpkg")
+setwd("/glade/scratch/kjfuller/data/GEDI")
+gedi = st_read("g2_f4_fhist.gpkg")
+targetcrs = st_crs(gedi)
 
 setwd("/glade/scratch/kjfuller/data")
 fesm = list.files("FESM_json", pattern = ".json")
-# identify crs using text-parsing
-t = jsonlite::read_json(paste0("FESM_json/",fesm[1]))
-s = t$crs$properties$name
-s = substr(s, 23, 27)
-# load geojson
-sf1 = geojsonsf::geojson_sf(paste0("FESM_json/", fesm[1]))
-sf1 = st_as_sf(sf1)
+g = extFESMfun(1)
 
-# assign crs manually
-st_crs(sf1) = as.numeric(s)
-
-# load severity classification raster
-r = list.files("FESM_img", pattern = unique(sf1$IncidentId))
-r = raster(paste0("FESM_img/", r[1]))
-
-# load RdNBR raster
-r3 = list.files("FESM_RdNBR", pattern = unique(sf1$IncidentId))
-r3 = raster(paste0("FESM_RdNBR/", r3[1]), band = 4)
-r2 = raster(r3)
-values(r2) = values(r3)
-rm(r3)
-
-# create raster extent polygon
-e <- extent(r)
-p <- as(e, 'SpatialPolygons') %>% st_as_sf()
-st_crs(p) = st_crs(r)
-
-# select GEDI shots and extract fire severity, remove NaNs and convert to crs of veg file
-g = st_transform(gedi, crs = st_crs(r))
-g = g[p,]
-g_buffer = st_buffer(g, dist = 12.5)
-
-g_temp = exact_extract(r, g_buffer, fun = "mode", append_cols = TRUE)
-names(g_temp)[names(g_temp) == "mode"] = "severity"
-g = left_join(g, g_temp)
-
-g = st_transform(g, crs = st_crs(r2))
-g_buffer = st_buffer(g, dist = 12.5)
-
-g_temp = exact_extract(r2, g_buffer, fun = "mode", append_cols = TRUE)
-names(g_temp)[names(g_temp) == "mode"] = "RdNBR"
-g = left_join(g, g_temp)
-
-g = g %>% 
-  filter(!is.na(severity))
-g = st_transform(g, crs = st_crs(veg))
-g$fire_sD = unique(sf1$StartDate)
-g$fire_id = unique(sf1$IncidentId)
-
-# process dates
-g$fire_sD = as.POSIXct(strptime(g$fire_sD, format = "%Y%m%d"))
-g = g %>% 
-  filter(Date < fire_sD)
-targetcrs = st_crs(g)
-g$lon = st_coordinates(g)[,1]
-g$lat = st_coordinates(g)[,2]
-st_geometry(g) = NULL
-
-rm(t, s, sf1, r, r2, e, p, g_temp, g_buffer, g_temp2)
 for(i in c((1 + 1):(1 + 99))){
   tryCatch({
-    # identify crs using text-parsing
-    t = jsonlite::read_json(paste0("FESM_json/",fesm[i]))
-    s = t$crs$properties$name
-    s = substr(s, 23, 27)
-    # load geojson
-    sf1 = geojsonsf::geojson_sf(paste0("FESM_json/", fesm[i]))
-    sf1 = st_as_sf(sf1)
-
-    # assign crs manually
-    st_crs(sf1) = as.numeric(s)
-
-    # load severity classification raster
-    r = list.files("FESM_img", pattern = unique(sf1$IncidentId))
-    r = raster(paste0("FESM_img/", r[1]))
-    
-    # load RdNBR raster
-    r3 = list.files("FESM_RdNBR", pattern = unique(sf1$IncidentId))
-    r3 = raster(paste0("FESM_RdNBR/", r3[1]), band = 4)
-    r2 = raster(r3)
-    values(r2) = values(r3)
-    rm(r3)
-
-    # create raster extent polygon
-    e <- extent(r)
-    p <- as(e, 'SpatialPolygons') %>% st_as_sf()
-    st_crs(p) = st_crs(r)
-
-    # select GEDI shots and extract fire severity, remove NaNs and convert to crs of veg file
-    g_temp = st_transform(gedi, crs = st_crs(r))
-    g_temp = g_temp[p,]
-    g_buffer = st_buffer(g_temp, dist = 12.5)
-    
-    g_temp2 = exact_extract(r, g_buffer, fun = "mode", append_cols = TRUE)
-    names(g_temp2)[names(g_temp2) == "mode"] = "severity"
-    g_temp = left_join(g_temp, g_temp2)
-    
-    g_temp = st_transform(g_temp, crs = st_crs(r2))
-    g_buffer = st_buffer(g_temp, dist = 12.5)
-    
-    g_temp2 = exact_extract(r2, g_buffer, fun = "mode", append_cols = TRUE)
-    names(g_temp2)[names(g_temp2) == "mode"] = "RdNBR"
-    g_temp = left_join(g_temp, g_temp2)
-    
-    g_temp = g_temp %>% 
-      filter(!is.na(severity))
-    g_temp = st_transform(g_temp, crs = st_crs(veg))
-    g_temp$fire_sD = unique(sf1$StartDate)
-    g_temp$fire_id = unique(sf1$IncidentId)
-    
-    # process dates
-    g_temp$fire_sD = as.POSIXct(strptime(g_temp$fire_sD, format = "%Y%m%d"))
-    g_temp = g_temp %>% 
-      filter(Date < fire_sD)
-    
-    # merge with whole file
-    g_temp$lon = st_coordinates(g_temp)[,1]
-    g_temp$lat = st_coordinates(g_temp)[,2]
-    st_geometry(g_temp) = NULL
+    g_temp = extFESMfun(i)
     if(nrow(g_temp) > 0){
       g = g %>% 
         full_join(g_temp)
     }
     
-    rm(t, s, sf1, r, r2, e, p, g_temp, g_buffer, g_temp2)
+    rm(g_temp)
   }, error = function(e){print(i); cat("ERROR :", conditionMessage(e), "\n")})
 }
 gedi = st_as_sf(g, coords = c("lon", "lat"), crs = targetcrs)
-setwd("/glade/scratch/kjfuller/data/GEDI_FESM")
-st_write(gedi, paste0("g2_f3_prefireshots_", code, "_12.5mbuffer.gpkg"), delete_dsn = TRUE)
+gedi_fires = gedi %>% 
+  filter(severity != 0)
+gedi_nofires = gedi %>% 
+  filter(severity == 0)
+setwd("/glade/scratch/kjfuller/data/chapter3")
+st_write(gedi_fires, paste0("ch3_g2_f3_shotsandFESM_fires_", code, ".gpkg"), delete_dsn = TRUE)
+st_write(gedi_nofires, paste0("ch3_g2_f3_shotsandFESM_nofires_", code, ".gpkg"), delete_dsn = TRUE)
