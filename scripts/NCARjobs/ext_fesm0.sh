8f33f9ba68417c0aa0ff3c521e00ca231873f26a
kjfuller06 <kjfuller06@gmail.com>
2022-08-15 14:14:49 -0500

    Initial commit

---
+++ scripts/NCARjobs/ext_fesm0.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -N ext_fesm0
+#PBS -A UWSY0001
+#PBS -l select=1:ncpus=1:mem=25GB
+#PBS -l walltime=05:00:00
+#PBS -q regular
+#PBS -j oe
+#PBS -k oed
+#PBS -r n
+
+### Temp data to scratch
+export TMPDIR=/glade/scratch/kjfuller/temp
+mkdir -p $TMPDIR
+
+### Load modules
+module load R/4.1.2
+module unload netcdf
+module load geos
+module load gdal
+module load proj
+module load peak_memusage
+
+### Run the executable
+peak_memusage.exe Rscript /glade/scratch/kjfuller/scripts/chapter3/ch3_extract_fesm_function.R --args code="001" num=1
