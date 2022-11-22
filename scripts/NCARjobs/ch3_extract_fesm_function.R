4794acafccef5b8baa9a55844ea5994475957b8b
kjfuller06 <kjfuller06@gmail.com>
2022-11-22 14:12:13 -0700

    Removed RdNBR extraction, added crs transformation earlier in the function, added another nrow(g) > 0 check and added a while() loop to create the first file for joining

--- scripts/NCARjobs/ch3_extract_fesm_function.R
+++ scripts/NCARjobs/ch3_extract_fesm_function.R
@@ -36,12 +36,6 @@ extFESMfun = function(x){
   # load raster
   r = list.files("FESM_img", pattern = unique(sf1$IncidentId))
   r = raster(paste0("FESM_img/", r[1]))
-  # load RdNBR raster
-  r3 = list.files("FESM_RdNBR", pattern = unique(sf1$IncidentId))
-  r3 = raster(paste0("FESM_RdNBR/", r3[1]), band = 4)
-  r2 = raster(r3)
-  values(r2) = values(r3)
-  rm(r3)
 
   # create raster extent polygon
   e <- extent(r)
@@ -59,35 +53,40 @@ extFESMfun = function(x){
     names(g_temp)[names(g_temp) == "mode"] = "severity"
     g = left_join(g, g_temp)
     
-    g = st_transform(g, crs = st_crs(r2))
-    g_buffer = st_buffer(g, dist = 12.5)
-    
-    g_temp = exact_extract(r2, g_buffer, fun = "mode", append_cols = TRUE)
-    names(g_temp)[names(g_temp) == "mode"] = "RdNBR"
-    g = left_join(g, g_temp)
+    g = st_transform(g, crs = targetcrs)
     
     # select only shots that intersect with fires- keep severity == 0 in case I need to use these as a control
-    g = g %>% 
-      filter(!is.na(severity))
-    g = st_transform(g, crs = targetcrs)
-    g$fire_eD = unique(sf1$EndDate)
-    g$fire_sD = unique(sf1$StartDate)
-    g$fire_id = unique(sf1$IncidentId)
-    
-    # process dates
-    g$fire_eD = as.POSIXct(strptime(g$fire_eD, format = "%Y%m%d"))
-    g$fire_sD = as.POSIXct(strptime(g$fire_sD, format = "%Y%m%d"))
     g = g %>%
-      filter(Date < fire_sD)
-    g$lon = st_coordinates(g)[,1]
-    g$lat = st_coordinates(g)[,2]
-    st_geometry(g) = NULL
+      filter(!is.na(severity))
+    
+    if(nrow(g) > 0){
+      g$fire_eD = unique(sf1$EndDate)
+      g$fire_sD = unique(sf1$StartDate)
+      g$fire_id = unique(sf1$IncidentId)
+      
+      # process dates
+      g$fire_eD = as.POSIXct(strptime(g$fire_eD, format = "%Y%m%d"))
+      g$fire_sD = as.POSIXct(strptime(g$fire_sD, format = "%Y%m%d"))
+      g = g %>%
+        filter(Date < fire_sD)
+      g$lon = st_coordinates(g)[,1]
+      g$lat = st_coordinates(g)[,2]
+      st_geometry(g) = NULL
+    } else {
+      g = data.frame()
+    }
+  } else {
+    g = data.frame()
   }
   return(g)
 }
-g = extFESMfun(num)
 
-for(i in c((num + 1):(end))){
+while(nrow(g) == 0){
+  g = extFESMfun(num)
+  num = num + 1
+}
+
+for(i in c((num):(end))){
   tryCatch({
     g_temp = extFESMfun(i)
     if(nrow(g_temp) > 0){
@@ -107,22 +106,32 @@ setwd("/glade/scratch/kjfuller/data/chapter3")
 st_write(gedi_fires, paste0("ch3_shotsandFESM_fires_", code, ".gpkg"), delete_dsn = TRUE)
 st_write(gedi_nofires, paste0("ch3_shotsandFESM_nofires_", code, ".gpkg"), delete_dsn = TRUE)
 
-# combine individual files
-## fires
-setwd("/glade/scratch/kjfuller/data/chapter3")
-fires = list.files(pattern = "_fires_")
-l = list()
-for(i in c(1:length(fires))){
-  l[[i]] = st_read(fires[[i]])
-}
-fires = rbind(l[[1]], l[[2]], l[[3]], l[[4]], l[[5]], l[[6]], l[[7]])
-st_write(fires, "ch3_shotsandFESM_fires.gpkg", delete_dsn = T)
-
-## no fires
-nofires = list.files(pattern = "_nofires_")
-l = list()
-for(i in c(1:length(nofires))){
-  l[[i]] = st_read(nofires[[i]])
-}
-nofires = rbind(l[[1]], l[[2]], l[[3]], l[[4]], l[[5]], l[[6]], l[[7]])
-st_write(nofires, "ch3_shotsandFESM_nofires.gpkg", delete_dsn = T)
+# # combine individual files
+# ## fires
+# setwd("/glade/scratch/kjfuller/data/chapter3")
+# fires = list.files(pattern = "_fires_")
+# l = list()
+# a = 1
+# for(i in c(1:length(fires))){
+#   l_temp = st_read(fires[[i]])
+#   if(nrow(l_temp) > 0){
+#     l[[a]] = l_temp
+#     a = a + 1
+#   }
+# }
+# fires = bind_rows(l)
+# st_write(fires, "ch3_shotsandFESM_fires.gpkg", delete_dsn = T)
+# 
+# ## no fires
+# nofires = list.files(pattern = "_nofires_")
+# l = list()
+# a = 1
+# for(i in c(1:length(nofires))){
+#   l_temp = st_read(nofires[[i]])
+#   if(nrow(l_temp) > 0){
+#     l[[a]] = l_temp
+#     a = a + 1
+#   }
+# }
+# nofires = bind_rows(l)
+# st_write(nofires, "ch3_shotsandFESM_nofires.gpkg", delete_dsn = T)
