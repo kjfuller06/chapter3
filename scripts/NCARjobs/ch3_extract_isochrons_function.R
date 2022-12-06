4a78e44f785da330fae4f6deff1187f97285e2a8
kjfuller06 <kjfuller06@gmail.com>
2022-12-06 11:49:40 -0700

    Filtered to exclude the first polygon and any fire spread over 12 hrs between measurements

--- scripts/NCARjobs/ch3_extract_isochrons_function.R
+++ scripts/NCARjobs/ch3_extract_isochrons_function.R
@@ -7,8 +7,9 @@ library(exactextractr)
 setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
 isochrons = st_read("progall_ffdi_v3.shp")
 isochrons$ID = c(1:nrow(isochrons))
+# isochrons$area_check = st_area(isochrons)
+## measurements are the same, do not apear to be cumulative- each polygon is measured separately; the variable "area" is in km
 
-# setwd("/glade/scratch/kjfuller/data/GEDI")
 setwd("E:/chapter3/GEDI_FESM")
 ext_progfun = function(x){
   # load data, create new polygons, intersecting the geometries of both
@@ -45,12 +46,14 @@ ext_progfun = function(x){
   
   gedi = left_join(g_buffer, gedi)
   gedi = st_as_sf(gedi, coords = c("lon", "lat"), crs = targetcrs)
+  
+  gedi$time = as.POSIXct(gedi$time)
+  gedi$lasttim = as.POSIXct(gedi$lasttim)
+  gedi$progtime = difftime(gedi$time, gedi$lasttim, units = "hours")
+  gedi$progtime = as.numeric(gedi$progtime)
+  gedi = gedi |> 
+    filter(progtime > 0) |> 
+    filter(progtime <= 12)
+  gedi$prog = gedi$area/gedi$progtime
   st_write(gedi, paste0("ch3_isochrons_prefire", x, ".gpkg"), delete_dsn = T)
 }
-
-ext_progfun(7)
-ext_progfun(14)
-ext_progfun(30)
-ext_progfun(60)
-ext_progfun(90)
-ext_progfun(180)
