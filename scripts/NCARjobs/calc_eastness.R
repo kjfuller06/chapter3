b8ad9b95acc3a3f631c49a710577037552e36d14
kjfuller06 <kjfuller06@gmail.com>
2022-12-06 16:55:24 -0700

    Corrected eastness calculation

--- scripts/NCARjobs/calc_eastness.R
+++ scripts/NCARjobs/calc_eastness.R
@@ -4,5 +4,5 @@ setwd("/glade/scratch/kjfuller/data")
 aspect = raster("proj_dem_aspect_30m.tif")
 
 eastness = aspect
-eastness = cos(eastness * pi / 180)
-writeRaster(eastness, "proj_dem_eastness_30m.tif")
+eastness = sin(eastness * pi / 180)
+writeRaster(eastness, "proj_dem_eastness_30m.tif", overwrite = T)
