3d1d5974e4c3e8d95100f5437465cf5c2b5be8f6
kjfuller06 <kjfuller06@gmail.com>
2022-12-09 15:54:31 -0700

    Added a section to process and rbind() sites that didn't burn at all during the FESM observation window

--- scripts/ch3_process_shots.R
+++ scripts/ch3_process_shots.R
@@ -8,7 +8,7 @@ library(lubridate)
 
 # setwd("/glade/scratch/kjfuller/data/GEDI_FESM")
 setwd("E:/chapter3/GEDI_FESM")
-# select unburned sites that were sampled no more than <x> days before fire ####
+# select burned sites that were sampled no more than <x> days before fire ####
 # # calculate TSF from FESM data, filter by 180 days
 # gedi = st_read("ch3_shotsandFESM_fires.gpkg")
 # targetcrs = st_crs(gedi)
@@ -36,48 +36,68 @@ setwd("E:/chapter3/GEDI_FESM")
 # ## FALSE
 # 
 # st_write(gedi_unburnt, "ch3_shotsandFESM_5yrsunburnt.gpkg")
-gedi_unburnt = st_read("ch3_shotsandFESM_5yrsunburnt.gpkg")
-
-shotfun = function(x){
-  gedi_prefire = aggregate(data = gedi_unburnt, TUF.fesm ~ shot_number, FUN = min) %>% # select the shortest TSF for each shot
-    filter(TUF.fesm < x) # select shots where the GEDI sample was no more than <x> days before fire
-  gedi_prefire = gedi_unburnt %>%  
-    filter(shot_number %in% gedi_prefire$shot_number)
-  st_write(gedi_prefire, paste0("ch3_shotsandFESM_prefire", x, ".gpkg"), delete_dsn = T)
-}
-
-shotfun(7)
-shotfun(14)
-shotfun(30)
-shotfun(60)
-shotfun(90)
-shotfun(180)
-
+g_temp = st_read("ch3_shotsandFESM_5yrsunburnt.gpkg")
+targetcrs = st_crs(g_temp)
+g_temp$lon = st_coordinates(g_temp)[,1]
+g_temp$lat = st_coordinates(g_temp)[,2]
+st_geometry(g_temp) = NULL
 
 # merge with fhist ####
 setwd("E:/chapter3/GEDI_fhist")
 fhist = st_read("ch3_f4_fhist.gpkg")
-targetcrs = st_crs(fhist)
-fhist$lon = st_coordinates(fhist)[,1]
-fhist$lat = st_coordinates(fhist)[,2]
 st_geometry(fhist) = NULL
 
-mergefun = function(x){
-  setwd("E:/chapter3/GEDI_FESM")
-  g_temp = st_read(paste0("ch3_shotsandFESM_prefire", x, ".gpkg"))
-  g_temp$lon = st_coordinates(g_temp)[,1]
-  g_temp$lat = st_coordinates(g_temp)[,2]
-  st_geometry(g_temp) = NULL
-  
-  g_temp = left_join(g_temp, fhist_temp) |> 
-    filter(TSF.hist >= 5 | is.na(TSF.hist))
-  g_temp = st_as_sf(g_temp, coords = c("lon", "lat"), crs = targetcrs)
-  st_write(g_temp, paste0("Ch3_allfiredata_prefire", x, ".gpkg"), delete_dsn = T)
-}
+setwd("E:/chapter3/GEDI_FESM")
+g_temp = left_join(g_temp, fhist) |> 
+  filter(TSF.hist >= 5 | is.na(TSF.hist))
+g_temp = st_as_sf(g_temp, coords = c("lon", "lat"), crs = targetcrs)
+st_write(g_temp, paste0("Ch3_FESMandfhist.gpkg"), delete_dsn = T)
 
-mergefun(7)
-mergefun(14)
-mergefun(30)
-mergefun(60)
-mergefun(90)
-mergefun(180)
+# select unburned sites ####
+# calculate TSF from FESM data, filter by 180 days
+gedi_unburnt = st_read("ch3_shotsandFESM_nofires.gpkg")
+targetcrs = st_crs(gedi_unburnt)
+
+any(duplicated(gedi_unburnt$shot_number))
+## TRUE
+
+gedi_unburnt = gedi_unburnt[!duplicated(gedi_unburnt$shot_number),]
+## remove duplicates- data are only for FESM fires, which did not occur for these shots
+any(duplicated(gedi_unburnt$shot_number))
+## FALSE
+names(gedi_unburnt)
+gedi_unburnt$fire_eD = NA
+gedi_unburnt$fire_sD = NA
+gedi_unburnt$fire_id = NA
+gedi_unburnt$TSF.fesm = NA
+gedi_unburnt$TUF.fesm = NA
+
+st_write(gedi_unburnt, "ch3_shotsandFESM_unburnt.gpkg")
+
+# merge with fhist ####
+setwd("E:/chapter3/GEDI_fhist")
+fhist = st_read("ch3_f4_fhist.gpkg")
+st_geometry(fhist) = NULL
+
+setwd("E:/chapter3/GEDI_FESM")
+g_temp = st_read("ch3_shotsandFESM_unburnt.gpkg")
+targetcrs = st_crs(g_temp)
+g_temp$lon = st_coordinates(g_temp)[,1]
+g_temp$lat = st_coordinates(g_temp)[,2]
+st_geometry(g_temp) = NULL
+
+g_temp = left_join(g_temp, fhist) |> 
+  filter(TSF.hist >= 5 | is.na(TSF.hist))
+g_temp = st_as_sf(g_temp, coords = c("lon", "lat"), crs = targetcrs)
+g_temp = g_temp |> 
+  filter(!shot_number %in% g_fires$shot_number)
+
+g_fires = st_read("Ch3_FESMandfhist.gpkg") |> 
+  dplyr::select(-n)
+names(g_fires)[names(g_fires) == "geom"] = "geometry"
+st_geometry(g_fires) = "geometry"
+
+g_temp = rbind(g_temp, g_fires)
+any(duplicated(g_temp$shot_number))
+
+st_write(g_temp, paste0("Ch3_FESMandfhist_forpoly.gpkg"), delete_dsn = T)
