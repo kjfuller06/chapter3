7941330593bfb130d17f7046e8cbd54d32e38ab7
kjfuller06 <kjfuller06@gmail.com>
2022-12-09 17:27:14 -0700

    Updated file names, selected shots based on fire dates first, then overlap; fire progression limited to 24 hours

--- scripts/NCARjobs/ch3_extract_isochrons_function_poly.R
+++ scripts/NCARjobs/ch3_extract_isochrons_function_poly.R
@@ -8,59 +8,48 @@ isochrons$ID = c(1:nrow(isochrons))
 # isochrons$area_check = st_area(isochrons)
 ## measurements are the same, do not apear to be cumulative- each polygon is measured separately; the variable "area" is in km
 
-setwd("E:/chapter3/GEDI")
+setwd("E:/chapter3/GEDI_FESM")
 # load data, create new polygons, intersecting the geometries of both
-gedi = st_read("ch3_f3_daterestricted.gpkg")
+gedi = st_read("ch3_FESMandfhist_forpoly.gpkg")
 targetcrs = st_crs(gedi)
 gedi = st_transform(gedi, st_crs(isochrons))
 g_buffer = st_buffer(gedi, dist = 12.5)
 g_buffer = st_intersection(g_buffer, isochrons)
-any(duplicated(g_buffer$shot_number))
-
-# select the shots that overlap more than one fire progression area
-dups = g_buffer[duplicated(g_buffer$shot_number),]
-dups = g_buffer |> 
-  filter(shot_number %in% dups$shot_number)
-# calculate the area of overlap and select only the largest overlapping progression area
-dups$overlap = st_area(dups)
-dups_max = aggregate(data = dups, overlap ~ shot_number, FUN = max)
-dups = left_join(dups_max, dups)
-st_geometry(dups) = "geom"
-
-# join all data together again
-dups = dups |> 
-  dplyr::select(-overlap)
+backup = g_buffer
+g_buffer$TUF = as.numeric(difftime(g_buffer$time, g_buffer$DateTime, units = "days"))
 g_buffer = g_buffer |> 
-  filter(!shot_number %in% dups$shot_number)
-g_buffer = rbind(g_buffer, dups)
-any(duplicated(g_buffer$shot_number))
-
-# merge extracted fire progression data to GEDI info and write to file
-st_geometry(g_buffer) = NULL
-gedi$lon = st_coordinates(gedi)[,1]
-gedi$lat = st_coordinates(gedi)[,2]
-st_geometry(gedi) = NULL
-
-gedi = left_join(g_buffer, gedi)
-gedi = st_as_sf(gedi, coords = c("lon", "lat"), crs = targetcrs)
-
-gedi$time = as.POSIXct(gedi$time)
-gedi$lasttim = as.POSIXct(gedi$lasttim)
-gedi$progtime = difftime(gedi$time, gedi$lasttim, units = "hours")
-gedi$progtime = as.numeric(gedi$progtime)
-gedi = gedi |> 
-  filter(progtime > 0) |> 
-  filter(progtime <= 12)
-gedi$prog = gedi$area/gedi$progtime
-
-gedi$TUF = as.numeric(difftime(gedi$time, gedi$DateTime, units = "days"))
-gedi = gedi |> 
   filter(TUF > 0)
 
 ext_progfun = function(x){
-  gedi_temp = gedi[gedi$TUF < x & gedi$TUF > 0,]
+  g_buffer = g_buffer[g_buffer$TUF < x,]
+  any(duplicated(g_buffer$shot_number))
   
-  st_write(gedi_temp, paste0("ch3_isochrons_prefire", x, "_poly.gpkg"), delete_dsn = T)
+  ## calculate the area of overlap and select only the largest overlapping progression area
+  g_buffer$overlap = st_area(g_buffer)
+  g_buffer_max = aggregate(data = g_buffer, overlap ~ shot_number, FUN = max)
+  g_buffer = left_join(g_buffer_max, g_buffer)
+  st_geometry(g_buffer) = "geom"
+  
+  # merge extracted fire progression data to GEDI info and write to file
+  st_geometry(g_buffer) = NULL
+  gedi$lon = st_coordinates(gedi)[,1]
+  gedi$lat = st_coordinates(gedi)[,2]
+  st_geometry(gedi) = NULL
+  
+  gedi = left_join(g_buffer, gedi)
+  gedi = st_as_sf(gedi, coords = c("lon", "lat"), crs = targetcrs)
+  
+  gedi$time = as.POSIXct(gedi$time)
+  gedi$lasttim = as.POSIXct(gedi$lasttim)
+  gedi$progtime = difftime(gedi$time, gedi$lasttim, units = "hours")
+  gedi$progtime = as.numeric(gedi$progtime)
+  gedi = gedi |> 
+    filter(progtime > 0) |> 
+    filter(progtime <= 24)
+  gedi$prog = gedi$area/gedi$progtime
+  any(duplicated(gedi$shot_number))
+  
+  st_write(gedi, paste0("ch3_isochrons_prefire", x, ".gpkg"), delete_dsn = T)
 }
 ext_progfun(7)
 ext_progfun(14)
