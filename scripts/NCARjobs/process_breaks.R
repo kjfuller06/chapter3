aeabdd473ddc66d4cfb1c42bc593cf84d29dd763
kjfuller06 <kjfuller06@gmail.com>
2022-12-13 14:42:17 -0700

    Initial commit

---
+++ scripts/NCARjobs/process_breaks.R
@@ -0,0 +1,47 @@
+library(raster)
+library(sf)
+library(tidyverse)
+library(geojsonsf)
+library(rjson)
+
+setwd("/glade/scratch/kjfuller/data")
+# setwd("E:/chapter3/fire lines/")
+firelines = geojson_sf("ClassifiedFireTrail_EPSG4326_edit.json")
+firelines$startdate = as.POSIXct(substr(firelines$startdate, 1, 8), format = "%Y%m%d")
+firelines = firelines |> 
+  filter(startdate < "2020-03-02")
+firelines$startdate[firelines$startdate < "2019-08-01"] = "2019-08-01"
+firelines = firelines |> 
+  dplyr::select(startdate) |>
+  group_by(startdate) %>%
+  summarise(do_union = FALSE) %>%
+  st_cast("MULTILINESTRING")
+
+# setwd("E:/chapter3/roadways/")
+roads = geojson_sf("RoadSegment_EPSG4326_edit.json")
+roads$startdate = as.POSIXct(substr(roads$startdate, 1, 8), format = "%Y%m%d")
+roads = roads |> 
+  filter(startdate < "2020-03-02") |> 
+  filter(roadontype != 3)
+roads$startdate[roads$startdate < "2019-08-01"] = "2019-08-01"
+roads = roads |> 
+  dplyr::select(startdate) |>
+  group_by(startdate) %>%
+  summarise(do_union = FALSE) %>%
+  st_cast("MULTILINESTRING")
+accesslines = rbind(firelines, roads)
+st_write(accesslines, "access_lines.gpkg", delete_dsn = T)
+
+# setwd("E:/chapter3/waterways/")
+water = geojson_sf("HydroArea_SPHERICAL_MERCATOR_edit.json")
+water$startdate = as.POSIXct(substr(water$startdate, 1, 8), format = "%Y%m%d")
+water = water |> 
+  filter(startdate < "2020-03-02") |> 
+  filter(perenniality == 1)
+water$startdate[water$startdate < "2019-08-01"] = "2019-08-01"
+water = water |> 
+  dplyr::select(startdate) |>
+  group_by(startdate) %>%
+  summarise(do_union = FALSE) %>%
+  st_cast("MULTIPOLYGON")
+st_write(water, "water.gpkg", delete_dsn = T)
---
+++ scripts/process_weather.R
@@ -0,0 +1,83 @@
+library(tidyverse)
+library(raster)
+library(sf)
+library(tmap)
+
+setwd("D:/chapter3/data")
+bom = read.csv(file = "BoMwindspeedstations.csv")
+stats = full_join(stats, bom) |> 
+  dplyr::select(Site, Lat, Lon)
+stats$agency = NA
+stats$agency[is.na(stats$Lat)] = "RFS"
+stats$agency[!is.na(stats$Lat)] = "BoM"
+stats = stats |> 
+  dplyr::select(agency, Site)
+names(stats)[2] = "stationID"
+stats = stats[order(stats$agency),]
+write.csv(stats, "WeatherStations.csv", row.names = F)
+
+setwd("E:/chapter3/from Michael")
+bom = st_read("available_100km_stations.shp")
+
+wind1 = read.csv("wind_data_100km_aws_stations.csv")
+## half-hour intervals
+unique(wind1$winddir_quality)
+## neither is quality checked
+wind1$date = as.Date(wind1$date_local)
+stats1 = aggregate(data = wind1, date ~ station, FUN = min)
+names(stats1)[2] = "aws_sd"
+stats2 = aggregate(data = wind1, date ~ station, FUN = max)
+names(stats2)[2] = "aws_ed"
+aws = full_join(stats1, stats2)
+any(duplicated(aws$station))
+
+wind2 = read.csv("wind_data_100km_synoptic_stations.csv")
+## 3-hr intervals
+unique(wind2$winddir_quality)
+## neither is quality checked
+wind2$date = as.Date(wind2$date_local)
+stats1 = aggregate(data = wind2, date ~ station, FUN = min)
+names(stats1)[2] = "syn_sd"
+stats2 = aggregate(data = wind2, date ~ station, FUN = max)
+names(stats2)[2] = "syn_ed"
+syn = full_join(stats1, stats2)
+any(duplicated(syn$station))
+
+stats = full_join(aws, syn)
+## dates match exactly where stations are present in both datasets, use aws first because of better temporal resolution
+
+bom = st_read("available_100km_stations.shp")
+targetcrs = st_crs(bom)
+bom$lon = st_coordinates(bom)[,1]
+bom$lat = st_coordinates(bom)[,2]
+st_geometry(bom) = NULL
+any(duplicated(bom$station))
+## TRUE
+bom = bom |> 
+  dplyr::select(station, lon, lat)
+bom = bom[!duplicated(bom$station),]
+
+wind1 = read.csv("wind_data_100km_aws_stations.csv")
+wind1$DateTime = as.POSIXct(paste0(wind1$date_local, " ", sprintf("%02.0f", wind1$hour_local), ":", sprintf("%02.0f", wind1$min_std)), format = "%Y-%m-%d %H:%M", tz = "UTC")
+wind1 = wind1 |> 
+  dplyr::select(station,
+                DateTime,
+                winddir) |> 
+  filter(!is.na(winddir))
+wind2 = read.csv("wind_data_100km_synoptic_stations.csv")
+wind2$DateTime = as.POSIXct(paste0(wind2$date_local, " ", sprintf("%02.0f", wind2$hour_local), ":", sprintf("%02.0f", wind2$min_std)), format = "%Y-%m-%d %H:%M",  tz = "UTC")
+wind2 = wind2 |>
+  dplyr::select(station,
+                DateTime,
+                winddir) |> 
+  filter(!is.na(winddir)) |> 
+  filter(!station %in% wind1$station)
+
+wind = rbind(wind1, wind2)
+wind$northness = cos(wind$winddir * pi / 180)
+wind$eastness = sin(wind$winddir * pi / 180)
+
+wind = wind |> 
+  left_join(bom)
+wind = st_as_sf(wind, coords = c("lon", "lat"), crs = targetcrs)
+st_write(wind, "wind_direction.gpkg", delete_dsn = T)
