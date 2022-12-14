1c35784163a2873e17c0feedcf189977653b16d0
kjfuller06 <kjfuller06@gmail.com>
2022-12-14 16:44:07 -0700

    Added access_lines, waterways and wind direction, completed wind direction and fire regime extraction code, corrected date feature

--- scripts/NCARjobs/ch3_extract_poly_predictors.R
+++ scripts/NCARjobs/ch3_extract_poly_predictors.R
@@ -2,6 +2,14 @@ library(sf)
 library(raster)
 library(tidyverse)
 library(exactextractr)
+library(rgeos)
+
+setwd("/glade/scratch/kjfuller/data")
+access = st_read("access_lines.gpkg")
+# setwd("E:/chapter3/waterways")
+water = st_read("water.gpkg")
+# setwd("E:/chapter3/from Michael")
+wind = st_read("wind_direction.gpkg")
 
 setwd("/glade/scratch/kjfuller/data/LFMC")
 # setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
@@ -15,27 +23,38 @@ vpd = list.files()
 vpd = data.frame(files = vpd)
 vpd$date = as.Date(substr(vpd$files, 5, 12), format = "%Y%m%d")
 
+setwd("/glade/scratch/kjfuller/data")
+fire_reg = read.csv("fire_regimes.csv") |> 
+  dplyr::select(fueltype,
+                fire_reg)
+
 setwd("/glade/scratch/kjfuller/data/chapter3")
 extractfun = function(x){
   # setwd("E:/chapter3/GEDI_FESM")
   gedi = st_read(paste0("ch3_isochrons_prefire", x, ".gpkg"))
+  targetcrs = st_crs(gedi)
   st_geometry(gedi) = NULL
-  gedi$fire_reg = as.factor(gedi$fire_reg)
+  gedi = gedi |> 
+    dplyr::select(shot_number,
+                  ID,
+                  rh98,
+                  cover_z_1,
+                  over_cover,
+                  fhd_normal)
   
   setwd("/glade/scratch/kjfuller/data")
   # setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
   g = st_read("progall_ffdi_v3.shp")
   g$ID = c(1:nrow(g))
-  targetcrs = st_crs(g)
-  g$poly_sD = as.Date(g$lasttim)
-  g$poly_eD = as.Date(g$time)
+  g$poly_sD = as.POSIXct(g$lasttim, format = "%Y-%m-%d %H:%M")
+  g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
   g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
   g$progtime = as.numeric(g$progtime)
   g = g |> 
     filter(progtime > 0) |> 
     filter(progtime <= 24)
   g$prog = g$area/g$progtime
-
+  
   g_agg = g |> 
     right_join(gedi)
   # calculate the median of continuous variables
@@ -45,17 +64,17 @@ extractfun = function(x){
   g_agg4 = aggregate(data = g_agg, fhd_normal ~ ID, FUN = median)
   
   g = g |> 
-    left_join(g_agg1) |> 
-    left_join(g_agg2) |> 
-    left_join(g_agg3) |> 
-    left_join(g_agg4)
-
+    right_join(g_agg1) |> 
+    right_join(g_agg2) |> 
+    right_join(g_agg3) |> 
+    right_join(g_agg4)
+  
   # load rasters
   # setwd("D:/chapter1/other_data/Final/terrain variables")
-
+  
   # elevation
   r = raster("proj_dem_s.tif")
-
+  
   g = st_transform(g, crs = st_crs(r))
   g$elevation = raster::extract(r, g, method = 'simple', fun = sd)
   
@@ -68,32 +87,46 @@ extractfun = function(x){
     filter(!is.na(elevation))
   g = rbind(g, g_na)
   
+  # fire regime
+  r = raster("fuels_30m.tif")
+  
+  g = st_transform(g, crs = st_crs(r))
+  g$fueltype = raster::extract(r, g, method = 'simple', fun = median)
+  g = g |> 
+    left_join(fire_reg)
+  
   # slope
   r = raster("proj_dem_slope_30m.tif")
   
   g = st_transform(g, crs = st_crs(r))
   g$slope = raster::extract(r, g, method = 'simple', fun = median)
   
+  # aspect
+  r = raster("proj_dem_aspect_30m.tif")
+  
+  g = st_transform(g, crs = st_crs(r))
+  g$aspect = raster::extract(r, g, method = 'simple', fun = median)
+  
   # northness
   r = raster("proj_dem_northness_30m.tif")
-
+  
   g = st_transform(g, crs = st_crs(r))
   g$northness = raster::extract(r, g, method = 'simple', fun = median)
   
   # eastness
   r = raster("proj_dem_eastness_30m.tif")
-
+  
   g = st_transform(g, crs = st_crs(r))
   g$eastness = raster::extract(r, g, method = 'simple', fun = median)
   
   # bark types
   r = raster("NSW_stringybark_distribution.tif")
-
+  
   g = st_transform(g, crs = st_crs(r))
   g$stringybark = raster::extract(r, g, method = 'simple', fun = median)
   
   r = raster("NSW_ribboning_distribution.tif")
-
+  
   g = st_transform(g, crs = st_crs(r))
   g$ribbonbark = raster::extract(r, g, method = 'simple', fun = median)
   
@@ -101,7 +134,7 @@ extractfun = function(x){
   g = st_transform(g, crs = targetcrs)
   st_write(g, paste0("ch3_forGAMs_poly_prefire", x, "_static.gpkg"), delete_dsn = T)
   # g = st_read(paste0("ch3_forGAMs_poly_prefire", x, "_static.gpkg"))
-
+  
   # LFMC
   setwd("/glade/scratch/kjfuller/data/LFMC")
   # setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
@@ -121,10 +154,10 @@ extractfun = function(x){
       filter(date == max(lfmc_temp$date))
     
     r = raster(lfmc_temp$files[1])
-
+    
     g_temp$LFMC = raster::extract(r, g_temp, method = 'simple', fun = median)
     g_temp$LFMC_sD = lfmc_temp$date[1]
-
+    
     if(any(is.na(g_temp$LFMC))){
       g_temp2 = g_temp |>
         filter(is.na(LFMC))
@@ -179,10 +212,88 @@ extractfun = function(x){
   g_temp = bind_rows(l)
   st_geometry(g_temp) = NULL
   g = full_join(g, g_temp)
-
+  
+  # wind direction
   g = st_transform(g, crs = targetcrs)
+  g_buffer = st_buffer(g, dist = 100000)
+  g_buffer = st_transform(g_buffer, st_crs(wind))
+  
+  # select just the stations for calculating distances
+  stations = wind |> 
+    dplyr::select(station)
+  stations = stations[!duplicated(stations$station),]
+  st_geometry(wind) = NULL
+  
+  l = list()
+  for(i in c(1:nrow(g))){
+    # select a buffered polygon and index all stations within 100km
+    g_temp = g_buffer[i,]
+    stat_temp = stations[g_temp,]
+    
+    # select the original polygon and calculate distance from centroid to selected stations
+    g_temp = g |> 
+      filter(ID %in% g_temp$ID)
+    g_center = st_centroid(g_temp)
+    g_center = st_transform(g_center, st_crs(g_buffer))
+    stat_temp$dist = as.numeric(st_distance(g_center, stat_temp))
+    st_geometry(stat_temp) = NULL
+    st_geometry(g_temp) = NULL
+    
+    # if the polygon is the first, overwrite poly_sD as 24hrs before poly_eD (in minutes)
+    if(min(g_temp$poly_sD) == max(g_temp$poly_eD)){
+      g_temp$poly_sD = g_temp$poly_eD - 86400
+    }
+    
+    # select wind data based on polygon timeframe
+    wind_temp = wind |> 
+      filter(station %in% stat_temp$station) |> 
+      left_join(stat_temp)
+    minT = wind_temp |> 
+      filter(DateTime <= min(g_temp$poly_sD))
+    minT = max(minT$DateTime, na.rm = T)
+    maxT = wind_temp |> 
+      filter(DateTime >= max(g_temp$poly_eD))
+    maxT = min(maxT$DateTime, na.rm = T)
+    wind_temp = wind_temp |> 
+      filter(DateTime >= minT) |> 
+      filter(DateTime <= maxT)
+    
+    # calculate the median wind speed within the polygon timeframe at each selected station, join to station data
+    wind_med = aggregate(data = wind_temp, winddir ~ station, FUN = median)
+    names(wind_med)[2] = "wind.median"
+    wind_var = aggregate(data = wind_temp, winddir ~ station, FUN = sd)
+    names(wind_var)[2] = "wind.stdev"
+    wind_temp = stat_temp |> 
+      inner_join(wind_med) |> 
+      inner_join(wind_var)
+    # write number of stations used for calculation
+    wind_temp$windstats = nrow(wind_med)
+    # create distance-based weights and calculate distance-weighted mean and wind impact index
+    wind_temp$weight = 1 - (wind_temp$dist)/max(wind_temp$dist+1000)
+    g_temp$windmean = weighted.mean(wind_temp$wind.median, wind_temp$weight)
+    g_temp$winddiff = g_temp$aspect - g_temp$windmean
+    g_temp$winddiff = cos(g_temp$winddiff * pi / 180)
+    # select relevant variables for passing to final df
+    g_temp = g_temp |> 
+      dplyr::select(shot_number,
+                    winddir,
+                    winddiff,
+                    windstats)
+    
+    g_temp = st_transform(g_temp, crs = targetcrs)
+    
+    l[[i]] = g_temp
+  }
+  g_temp = bind_rows(l)
+  g = full_join(g, g_temp)
+  
+  # distance to water
+  extent(g)
+  r_temp = raster(ext = extent(g), res = 30, crs = crs(g))
+  dd = gDistance(water, as(r_temp,"SpatialPoints"), byid=TRUE) ## need to trouble-shoot, need to subset by date created
+  
   setwd("/glade/scratch/kjfuller/data/chapter3")
-  st_write(g, paste0("ch3_forGAMs_prefire", x, "_allvars.gpkg"), delete_dsn = T)
+  st_write(g, paste0("ch3_forGAMs_prefire", x, "_poly_allvars.gpkg"), delete_dsn = T)
 }
 
 extractfun(7)
@@ -192,4 +303,5 @@ extractfun(60)
 extractfun(90)
 extractfun(180)
 
-## still need to extract: fire regime types (in order to restrict less representative veg types), fire type categories (FESM directly), distance to roads, distance to water
+## still need to extract: fire type categories (FESM directly), distance to roads (or number of dwellings), distance to water 
+## aspect, wind direction done, fire regime types (in order to restrict less representative veg types) done
