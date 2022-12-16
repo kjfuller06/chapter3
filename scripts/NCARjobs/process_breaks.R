d233366487c1710322b88bb8a74960bdc88b8c9b
kjfuller06 <kjfuller06@gmail.com>
2022-12-15 17:17:40 -0700

    Changed polygon filter to include fire progression polygons of 8 days, instead of 24 hrs

--- scripts/NCARjobs/process_breaks.R
+++ scripts/NCARjobs/process_breaks.R
@@ -60,9 +60,9 @@ g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
 g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
 g$progtime = as.numeric(g$progtime)
 # filter polygons
-g = g |> 
-  filter(progtime > 0) |> 
-  filter(progtime <= 24)
+g = g |>
+  filter(progtime > 0) |>
+  filter(progtime <= 192) ## progression time < 8 days (8 * 24 = 192)
 g = g |> 
   dplyr::select(ID, poly_sD)
 
@@ -114,21 +114,21 @@ for(i in c(1:nrow(g))){
 l = bind_rows(l)
 write.csv(l, "isochron_breaks.csv", row.names = F)
 
-# # housing density ####
-# # setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
-# g = st_read("progall_ffdi_v3.shp")
-# targetcrs = st_crs(g)
-# g$ID = c(1:nrow(g))
-# g$poly_sD = as.POSIXct(g$lasttim, format = "%Y-%m-%d %H:%M")
-# g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
-# g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
-# g$progtime = as.numeric(g$progtime)
-# g = g |> 
-#   filter(progtime > 0) |> 
-#   filter(progtime <= 24)
-# g = g |> 
-#   dplyr::select(ID, poly_sD, area)
-# 
+# housing density ####
+# setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
+g = st_read("progall_ffdi_v3.shp")
+targetcrs = st_crs(g)
+g$ID = c(1:nrow(g))
+g$poly_sD = as.POSIXct(g$lasttim, format = "%Y-%m-%d %H:%M")
+g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
+g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
+g$progtime = as.numeric(g$progtime)
+g = g |>
+  filter(progtime > 0) |>
+  filter(progtime <= 192) ## progression time < 8 days (8 * 24 = 192)
+g = g |>
+  dplyr::select(ID, poly_sD, area)
+
 # # setwd("E:/chapter3/dwellings")
 # sf::sf_use_s2(FALSE)
 # houses = geojson_sf("Property_EPSG4326_edit.json")
@@ -136,27 +136,28 @@ write.csv(l, "isochron_breaks.csv", row.names = F)
 # houses = houses |>
 #   filter(startdate < "2020-03-02") |> filter(propertytype == 1) |>
 #   dplyr::select(startdate) |>
-#   st_transform(crs = targetcrs) |> 
+#   st_transform(crs = targetcrs) |>
 #   st_centroid()
 # st_write(houses, "houses.gpkg", delete_dsn = T)
-# 
-# # houses = st_read("houses.gpkg")
-# # sf::sf_use_s2(FALSE)
-# 
-# houses = st_intersection(houses, g)
-# st_write(houses, "houses_isochrons.gpkg", delete_dsn = T)
-# 
-# st_geometry(houses) = NULL
-# houses = houses |> 
-#   filter(startdate < poly_sD) 
-# 
-# house_agg = houses |> 
-#   group_by(ID) |> 
-#   tally() |> 
-#   as.data.frame()
-# house_id = houses |> 
-#   dplyr::select(ID, area) |> 
-#   unique()
-# houses = full_join(house_agg, house_id)
-# houses$house.density = houses$n/houses$area
-# write.csv(houses, "housing_density.csv", row.names = F)
+
+houses = st_read("houses.gpkg")
+houses$startdate = as.POSIXct(houses$startdate, 1, 8)
+sf::sf_use_s2(FALSE)
+
+houses = st_intersection(houses, g)
+st_write(houses, "houses_isochrons.gpkg", delete_dsn = T)
+
+st_geometry(houses) = NULL
+houses = houses |>
+  filter(startdate < poly_sD)
+
+house_agg = houses |>
+  group_by(ID) |>
+  tally() |>
+  as.data.frame()
+house_id = houses |>
+  dplyr::select(ID, area) |>
+  unique()
+houses = full_join(house_agg, house_id)
+houses$house.density = houses$n/houses$area
+write.csv(houses, "housing_density.csv", row.names = F)
