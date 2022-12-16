431cc5c791623568d20cd2e26bc99d445a30f7a7
kjfuller06 <kjfuller06@gmail.com>
2022-12-15 17:04:46 -0700

    Calculated housing density and distance to 1) roads or firelines and 2) bodies of water

--- scripts/NCARjobs/process_breaks.R
+++ scripts/NCARjobs/process_breaks.R
@@ -3,8 +3,10 @@ library(sf)
 library(tidyverse)
 library(geojsonsf)
 library(rjson)
+library(rgeos)
 
 setwd("/glade/scratch/kjfuller/data")
+# # firelines ####
 # # setwd("E:/chapter3/fire lines/")
 # firelines = geojson_sf("ClassifiedFireTrail_EPSG4326_edit.json")
 # firelines$startdate = as.POSIXct(substr(firelines$startdate, 1, 8), format = "%Y%m%d")
@@ -17,6 +19,7 @@ setwd("/glade/scratch/kjfuller/data")
 #   summarise(do_union = FALSE) %>%
 #   st_cast("MULTILINESTRING")
 # 
+# # roads ####
 # # setwd("E:/chapter3/roadways/")
 # roads = geojson_sf("RoadSegment_EPSG4326_edit.json")
 # roads$startdate = as.POSIXct(substr(roads$startdate, 1, 8), format = "%Y%m%d")
@@ -32,6 +35,7 @@ setwd("/glade/scratch/kjfuller/data")
 # accesslines = rbind(firelines, roads)
 # st_write(accesslines, "access_lines.gpkg", delete_dsn = T)
 # 
+# # water ####
 # # setwd("E:/chapter3/waterways/")
 # water = geojson_sf("HydroArea_SPHERICAL_MERCATOR_edit.json")
 # water$startdate = as.POSIXct(substr(water$startdate, 1, 8), format = "%Y%m%d")
@@ -46,15 +50,113 @@ setwd("/glade/scratch/kjfuller/data")
 #   st_cast("MULTIPOLYGON")
 # st_write(water, "water.gpkg", delete_dsn = T)
 
-# setwd("E:/chapter3/dwellings")
-sf::sf_use_s2(FALSE)
-houses = geojson_sf("Property_EPSG4326_edit.json")
-houses$startdate = as.POSIXct(substr(houses$startdate, 1, 8), format = "%Y%m%d")
-houses = houses |> 
-  dplyr::select(startdate) |> 
-  filter(startdate < "2020-03-02") |> filter(propertytype == 1) |> 
-  st_centroid() |> 
-  group_by(startdate) %>%
-  summarise(do_union = FALSE) %>%
-  st_cast("MULTIPOINT")
-st_write(houses, "houses.gpkg", delete_dsn = T)
+# # distance to breaks ####
+# setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
+g = st_read("progall_ffdi_v3.shp")
+targetcrs = st_crs(g)
+g$ID = c(1:nrow(g))
+g$poly_sD = as.POSIXct(g$lasttim, format = "%Y-%m-%d %H:%M")
+g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
+g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
+g$progtime = as.numeric(g$progtime)
+# filter polygons
+g = g |> 
+  filter(progtime > 0) |> 
+  filter(progtime <= 24)
+g = g |> 
+  dplyr::select(ID, poly_sD)
+
+# convert features back to simple geometries, remove Z axis and convert to projected CRS
+# setwd("E:/chapter3/waterways/")
+water = st_read("water.gpkg")
+water = st_zm(water, drop = TRUE, what = "ZM")
+water = st_cast(water, "POLYGON")
+water = st_transform(water, crs = targetcrs)
+# setwd("E:/chapter3/roadways/")
+access = st_read("access_lines.gpkg")
+access = st_zm(access, drop = TRUE, what = "ZM")
+access = st_cast(access, "LINESTRING")
+access = st_transform(access, crs = targetcrs)
+
+# for each polygon, create a raster of regular cells across the surface, filter features by start date and sample the distance from each raster cell to each feature
+l = list()
+for(i in c(1:nrow(g))){
+  g_temp = g[g$ID == unique(g$ID)[i],]
+  r_temp = raster(ext = extent(g_temp), res = 10, crs = crs(g))
+  
+  water_temp = water |> 
+    filter(startdate < min(g_temp$poly_sD))
+  water_temp = as(water_temp, "Spatial")
+  
+  access_temp = access |> 
+    filter(startdate < min(g_temp$poly_sD))
+  access_temp = as(access_temp, "Spatial")
+  
+  waterMin = 
+        gDistance(access_temp, as(r_temp,"SpatialPoints"), byid=TRUE)
+  accessMin = 
+          gDistance(water_temp, as(r_temp,"SpatialPoints"), byid=TRUE)
+  ## dd is a matrix with ncol == nrow(water_temp) and nrow == ncell(r_temp)
+  ## columns correspond to polygons or lines, rows correspond to systematically chosen points in the selected polygon
+  ## calculate distance between water and raster cells
+  
+  df = cbind(waterMin, accessMin)
+  g_temp$breaksMin = 
+    median(as.numeric(
+      apply(df, 1, min)))
+  ## combine distances by cell, calculate the minimum for each row (each cell), calculate the median min distance to a break for each polygon
+  
+  st_geometry(g_temp) = NULL
+  g_temp = g_temp |> 
+    dplyr::select(ID, breaksMin)
+  l[[i]] = g_temp
+}
+l = bind_rows(l)
+write.csv(l, "isochron_breaks.csv", row.names = F)
+
+# # housing density ####
+# # setwd("E:/chapter3/isochrons/Progall_ffdi_v3")
+# g = st_read("progall_ffdi_v3.shp")
+# targetcrs = st_crs(g)
+# g$ID = c(1:nrow(g))
+# g$poly_sD = as.POSIXct(g$lasttim, format = "%Y-%m-%d %H:%M")
+# g$poly_eD = as.POSIXct(g$time, format = "%Y-%m-%d %H:%M")
+# g$progtime = difftime(g$poly_eD, g$poly_sD, units = "hours")
+# g$progtime = as.numeric(g$progtime)
+# g = g |> 
+#   filter(progtime > 0) |> 
+#   filter(progtime <= 24)
+# g = g |> 
+#   dplyr::select(ID, poly_sD, area)
+# 
+# # setwd("E:/chapter3/dwellings")
+# sf::sf_use_s2(FALSE)
+# houses = geojson_sf("Property_EPSG4326_edit.json")
+# houses$startdate = as.POSIXct(substr(houses$startdate, 1, 8), format = "%Y%m%d")
+# houses = houses |>
+#   filter(startdate < "2020-03-02") |> filter(propertytype == 1) |>
+#   dplyr::select(startdate) |>
+#   st_transform(crs = targetcrs) |> 
+#   st_centroid()
+# st_write(houses, "houses.gpkg", delete_dsn = T)
+# 
+# # houses = st_read("houses.gpkg")
+# # sf::sf_use_s2(FALSE)
+# 
+# houses = st_intersection(houses, g)
+# st_write(houses, "houses_isochrons.gpkg", delete_dsn = T)
+# 
+# st_geometry(houses) = NULL
+# houses = houses |> 
+#   filter(startdate < poly_sD) 
+# 
+# house_agg = houses |> 
+#   group_by(ID) |> 
+#   tally() |> 
+#   as.data.frame()
+# house_id = houses |> 
+#   dplyr::select(ID, area) |> 
+#   unique()
+# houses = full_join(house_agg, house_id)
+# houses$house.density = houses$n/houses$area
+# write.csv(houses, "housing_density.csv", row.names = F)
