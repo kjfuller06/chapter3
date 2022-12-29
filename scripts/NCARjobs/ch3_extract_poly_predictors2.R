56ffa1c17dad449df838c5eb39aaabd8b96d1887
kjfuller06 <kjfuller06@gmail.com>
2022-12-29 11:46:22 -0700

    Removed st_geometry() causing problems

--- scripts/NCARjobs/ch3_extract_poly_predictors2.R
+++ scripts/NCARjobs/ch3_extract_poly_predictors2.R
@@ -4,12 +4,12 @@ library(tidyverse)
 library(exactextractr)
 library(rgeos)
 
-setwd("/glade/scratch/kjfuller/data/LFMC")
-# setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
-lfmc = list.files()
-lfmc = data.frame(files = lfmc)
-lfmc$date = as.POSIXct(substr(lfmc$files, 6, 15), format = "%Y_%m_%d")
-## using as.Date led to problems for this extraction but the severity data extraction did fine- double-checked
+# setwd("/glade/scratch/kjfuller/data/LFMC")
+# # setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
+# lfmc = list.files()
+# lfmc = data.frame(files = lfmc)
+# lfmc$date = as.POSIXct(substr(lfmc$files, 6, 15), format = "%Y_%m_%d")
+# ## using as.Date led to problems for this extraction but the severity data extraction did fine- double-checked
 
 setwd("/glade/scratch/kjfuller/data/VPD")
 # setwd("E:/chapter3/from Rachael/VPD")
@@ -17,108 +17,109 @@ vpd = list.files()
 vpd = data.frame(files = vpd)
 vpd$date = as.POSIXct(substr(vpd$files, 5, 12), format = "%Y%m%d")
 
-# load isochrons ####
+# # load isochrons ####
+# setwd("/glade/scratch/kjfuller/data/chapter3")
+# # setwd("E:/chapter3/for GAMs")
+# g = st_read("ch3_forGAMs_poly_prefire180_structure.gpkg")
+# targetcrs = st_crs(g)
+# g = g |> 
+#   dplyr::select(ID,
+#                 poly_sD,
+#                 poly_eD)
+# 
+# # LFMC ####
+# setwd("/glade/scratch/kjfuller/data/LFMC")
+# # setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
+# r = raster(lfmc$files[1])
+# g = st_transform(g, crs = st_crs(r))
+# ids = unique(g$ID)
+# 
+# counter <- 0
+# l <- list(NULL)
+# size <- 1
+# dynamicfun = function(x){
+#   if( .GlobalEnv$counter == .GlobalEnv$size )
+#   {length(.GlobalEnv$l) <- .GlobalEnv$size <- .GlobalEnv$size * 2}
+#   
+#   g_temp = g |>
+#     filter(ID == ids[x])
+#   
+#   ## select LFMC values that were calculated for dates before the fire started
+#   lfmc_temp = lfmc |>
+#     filter(as.numeric(difftime(min(g_temp$poly_sD), date)) > 0)
+#   ## select the last possible pre-fire LFMC value
+#   minT = lfmc_temp |>
+#     filter(date == max(lfmc_temp$date))
+#   
+#   ## select LFMC values that were calculated for dates before the fire end
+#   lfmc_temp = lfmc |> 
+#     filter(as.numeric(difftime(max(g_temp$poly_eD), date)) > 0)
+#   ## select the last possible during-fire LFMC value
+#   maxT = lfmc_temp |>
+#     filter(date == max(lfmc_temp$date))
+#   
+#   lfmc_temp = lfmc |> 
+#     filter(date >= minT$date & date <= maxT$date)
+#   
+#   l = list()
+#   if(nrow(lfmc_temp) > 0){
+#     for(v in c(1:nrow(lfmc_temp))){
+#       r_temp = raster(lfmc_temp$files[v])
+#       l[[v]] = raster::extract(r_temp, g_temp, method = 'simple')
+#     }
+#     g_temp$LFMC = median(unlist(l), na.rm = T)
+#   } else {
+#     r = raster(lfmc_temp$files[1])
+#     g_temp$LFMC = raster::extract(r, g_temp, method = 'simple', fun = median, na.rm = T)
+#   }
+#   g_temp$LFMC_sD = lfmc_temp$date[1]
+#   
+#   if(any(is.na(g_temp$LFMC))){
+#     g_temp2 = g_temp |>
+#       filter(is.na(LFMC))
+#     if(g_temp2$LFMC_sD != min(lfmc$date)){
+#       print("LFMC values were NA, attempting to extract earlier LFMC values")
+#       lfmc_temp = lfmc |>
+#         filter(date < g_temp2$LFMC_sD[1])
+#       lfmc_temp = lfmc_temp |>
+#         filter(date == max(lfmc_temp$date))
+#       
+#       r = raster(lfmc_temp$files[1])
+#       
+#       g_temp2$LFMC = raster::extract(r, g_temp2, method = 'simple', fun = median, na.rm = T)
+#       g_temp2$LFMC_sD = lfmc_temp$date[1]
+#       
+#       if(any(!is.na(g_temp2$LFMC))){
+#         print("Earlier LFMC value extracted, at least some values not NA")
+#         g_temp = g_temp |>
+#           filter(!is.na(LFMC)) |>
+#           rbind(g_temp2)
+#       } else {
+#         print("Earlier LFMC values were also NA")
+#       }
+#     }
+#   }
+#   # l[[i]] = g_temp
+#   
+#   st_geometry(g_temp) = NULL
+#   
+#   .GlobalEnv$counter <- .GlobalEnv$counter + 1
+#   .GlobalEnv$l[[.GlobalEnv$counter]] <- g_temp
+# }
+# for(i in c(1:length(ids))){
+#   dynamicfun(i)
+#   print(paste0("LFMC ", i))
+# }
+# 
+# g_temp = bind_rows(l)
+# g = left_join(g, g_temp) |> 
+#   st_transform(crs = targetcrs)
+
 setwd("/glade/scratch/kjfuller/data/chapter3")
 # setwd("E:/chapter3/for GAMs")
-g = st_read("ch3_forGAMs_poly_prefire180_structure.gpkg")
+# st_write(g, "ch3_forGAMs_poly_prefire180_LFMC.gpkg", delete_dsn = T)
+g = st_read("ch3_forGAMs_poly_prefire180_LFMC.gpkg")
 targetcrs = st_crs(g)
-g = g |> 
-  dplyr::select(ID,
-                poly_sD,
-                poly_eD)
-
-# LFMC ####
-setwd("/glade/scratch/kjfuller/data/LFMC")
-# setwd("E:/chapter3/from Rachael/R project/LFMC/LFMC/GEE_LFMC/LFMC_rasters/GEE_v20May2020_RevisedMask/")
-r = raster(lfmc$files[1])
-g = st_transform(g, crs = st_crs(r))
-ids = unique(g$ID)
-
-counter <- 0
-l <- list(NULL)
-size <- 1
-dynamicfun = function(x){
-  if( .GlobalEnv$counter == .GlobalEnv$size )
-  {length(.GlobalEnv$l) <- .GlobalEnv$size <- .GlobalEnv$size * 2}
-  
-  g_temp = g |>
-    filter(ID == ids[x])
-  
-  ## select LFMC values that were calculated for dates before the fire started
-  lfmc_temp = lfmc |>
-    filter(as.numeric(difftime(min(g_temp$poly_sD), date)) > 0)
-  ## select the last possible pre-fire LFMC value
-  minT = lfmc_temp |>
-    filter(date == max(lfmc_temp$date))
-  
-  ## select LFMC values that were calculated for dates before the fire end
-  lfmc_temp = lfmc |> 
-    filter(as.numeric(difftime(max(g_temp$poly_eD), date)) > 0)
-  ## select the last possible during-fire LFMC value
-  maxT = lfmc_temp |>
-    filter(date == max(lfmc_temp$date))
-  
-  lfmc_temp = lfmc |> 
-    filter(date >= minT$date & date <= maxT$date)
-  
-  l = list()
-  if(nrow(lfmc_temp) > 0){
-    for(v in c(1:nrow(lfmc_temp))){
-      r_temp = raster(lfmc_temp$files[v])
-      l[[v]] = raster::extract(r_temp, g_temp, method = 'simple')
-    }
-    g_temp$LFMC = median(unlist(l), na.rm = T)
-  } else {
-    r = raster(lfmc_temp$files[1])
-    g_temp$LFMC = raster::extract(r, g_temp, method = 'simple', fun = median, na.rm = T)
-  }
-  g_temp$LFMC_sD = lfmc_temp$date[1]
-  
-  if(any(is.na(g_temp$LFMC))){
-    g_temp2 = g_temp |>
-      filter(is.na(LFMC))
-    if(g_temp2$LFMC_sD != min(lfmc$date)){
-      print("LFMC values were NA, attempting to extract earlier LFMC values")
-      lfmc_temp = lfmc |>
-        filter(date < g_temp2$LFMC_sD[1])
-      lfmc_temp = lfmc_temp |>
-        filter(date == max(lfmc_temp$date))
-      
-      r = raster(lfmc_temp$files[1])
-      
-      g_temp2$LFMC = raster::extract(r, g_temp2, method = 'simple', fun = median, na.rm = T)
-      g_temp2$LFMC_sD = lfmc_temp$date[1]
-      
-      if(any(!is.na(g_temp2$LFMC))){
-        print("Earlier LFMC value extracted, at least some values not NA")
-        g_temp = g_temp |>
-          filter(!is.na(LFMC)) |>
-          rbind(g_temp2)
-      } else {
-        print("Earlier LFMC values were also NA")
-      }
-    }
-  }
-  # l[[i]] = g_temp
-  
-  st_geometry(g_temp) = NULL
-  
-  .GlobalEnv$counter <- .GlobalEnv$counter + 1
-  .GlobalEnv$l[[.GlobalEnv$counter]] <- g_temp
-}
-for(i in c(1:length(ids))){
-  dynamicfun(i)
-  print(paste0("LFMC ", i))
-}
-
-g_temp = bind_rows(l)
-g = left_join(g, g_temp) |> 
-  st_transform(crs = targetcrs)
-
-setwd("/glade/scratch/kjfuller/data/chapter3")
-# setwd("E:/chapter3/for GAMs")
-st_write(g, "ch3_forGAMs_poly_prefire180_LFMC.gpkg", delete_dsn = T)
-# g = st_read("ch3_forGAMs_poly_prefire180_LFMC.gpkg")
 
 # VPD ####
 setwd("/glade/scratch/kjfuller/data/VPD")
@@ -180,7 +181,6 @@ for(i in c(1:length(ids))){
 }
 
 g_temp = bind_rows(l)
-st_geometry(g_temp) = NULL
 g = left_join(g, g_temp) |> 
   st_transform(crs = targetcrs)
 
