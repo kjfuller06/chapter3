466e57d634a9df76e1babba255c978d915dfd378
kjfuller06 <kjfuller06@gmail.com>
2023-01-18 00:47:24 -0700

    Initial commit

---
+++ scripts/ch3_extract_poly_structure.R
@@ -0,0 +1,48 @@
+library(sf)
+library(raster)
+library(tidyverse)
+library(exactextractr)
+library(rgeos)
+
+# load forest structure data ####
+# setwd("/glade/scratch/kjfuller/data/chapter3")
+setwd("E:/chapter3/GEDI_FESM/")
+gedi = st_read(paste0("ch3_isochrons_prefire180.gpkg"))
+targetcrs = st_crs(gedi)
+st_geometry(gedi) = NULL
+gedi = gedi |> 
+  dplyr::select(shot_number,
+                ID,
+                rh98,
+                cover,
+                cover_z_1,
+                over_cover,
+                fhd_normal)
+
+# load isochrons ####
+# setwd("/glade/scratch/kjfuller/data")
+setwd("E:/chapter3/isochrons")
+g = st_read("isochrons_8days.gpkg")
+
+# calculate and merge structural data ####
+g_agg = g |> 
+  inner_join(gedi)
+# calculate the median of continuous variables
+g_agg1 = aggregate(data = g_agg, rh98 ~ ID, FUN = median)
+g_agg2 = aggregate(data = g_agg, cover ~ ID, FUN = median)
+g_agg3 = aggregate(data = g_agg, cover_z_1 ~ ID, FUN = median)
+g_agg4 = aggregate(data = g_agg, over_cover ~ ID, FUN = median)
+g_agg5 = aggregate(data = g_agg, fhd_normal ~ ID, FUN = median)
+
+print("joining all median values of GEDI structure to isochron data")
+g = g |> 
+  right_join(g_agg1) |> 
+  right_join(g_agg2) |> 
+  right_join(g_agg3) |> 
+  right_join(g_agg4) |> 
+  right_join(g_agg5)
+
+# setwd("/glade/scratch/kjfuller/data/chapter3")
+setwd("E:/chapter3/for GAMs")
+g = st_transform(g, crs = targetcrs)
+st_write(g, paste0("ch3_forGAMs_poly_prefire180_structure_redo.gpkg"), delete_dsn = T)
