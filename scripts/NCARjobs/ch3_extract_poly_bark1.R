b48f66000e7ced9955b603f38f86b77f1b3ba2b5
kjfuller06 <kjfuller06@gmail.com>
2023-01-27 22:49:15 -0700

    Updated to latest files

--- scripts/NCARjobs/ch3_extract_poly_bark1.R
+++ scripts/NCARjobs/ch3_extract_poly_bark1.R
@@ -4,24 +4,34 @@ library(tidyverse)
 library(exactextractr)
 library(rgeos)
 
-# # load isochrons 
-# setwd("/glade/scratch/kjfuller/data/chapter3")
-# # setwd("E:/chapter3/for GAMs")
-# g = st_read("ch3_forGAMs_poly_prefire180_structure.gpkg")
-# targetcrs = st_crs(g)
-# g = g |> 
-#   dplyr::select(ID)
-# 
-# setwd("/glade/scratch/kjfuller/data")
-# # setwd("D:/chapter1/bark-type-SDM/outputs/RFs/final models/final geotiffs")
+# load isochrons
+setwd("/glade/scratch/kjfuller/data/chapter3")
+# setwd("E:/chapter3/for GAMs")
+g = st_read("ch3_forGAMs_poly_prefire180_structure.gpkg")
+targetcrs = st_crs(g)
+g = g |>
+  dplyr::select(ID)
+
+setwd("/glade/scratch/kjfuller/data")
+# setwd("D:/chapter1/bark-type-SDM/outputs/RFs/final models/final geotiffs")
 # r = raster("NSW_stringybark_distribution.tif")
-# 
-# g = st_transform(g, crs = st_crs(r))
-# g$stringybark = raster::extract(r, g, method = 'simple', fun = median, na.rm = T)
-# 
-# setwd("/glade/scratch/kjfuller/data/chapter3")
-# g = st_transform(g, crs = targetcrs)
-# st_write(g, paste0("ch3_forGAMs_poly_prefire180_bark1.gpkg"), delete_dsn = T)
+r = raster("predmosaic_RF5_stringybark_07_allfires.tif")
+threshold = 0.6 ## values are inverted for stringybark
+# values(rpred)[values(rpred) > threshold] = 0
+# values(rpred)[values(rpred) <= threshold] = 1
+rcl = matrix(c(-Inf, 0.4,
+               0.4, Inf,
+               0, 1), 
+             ncol = 3, byrow = F)
+r = reclassify(x = r, rcl = rcl, right = F, include.lowest = T)
+writeRaster(r, "RF5_stringybark_classified.tif", overwrite = T)
+
+g = st_transform(g, crs = st_crs(r))
+g$stringybark = raster::extract(r, g, method = 'simple', fun = median, na.rm = T)
+
+setwd("/glade/scratch/kjfuller/data/chapter3")
+g = st_transform(g, crs = targetcrs)
+st_write(g, paste0("ch3_forGAMs_poly_prefire180_bark1.gpkg"), delete_dsn = T)
 
 # load isochrons ####
 setwd("/glade/scratch/kjfuller/data/chapter3")
