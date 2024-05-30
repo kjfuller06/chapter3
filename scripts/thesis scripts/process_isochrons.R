26a5594ca851795807734ee61a262c6261dc3fa5
kjfuller06 <kjfuller06@gmail.com>
2024-05-30 16:08:53 -0600

    messing around, didn't end up using it

--- scripts/thesis scripts/process_isochrons.R
+++ scripts/thesis scripts/process_isochrons.R
@@ -1,7 +1,7 @@
 library(raster)
 library(sf)
 library(tidyverse)
-# library(tmap)
+library(tmap)
 # library(spdep)
 
 # # validate geometries ####
@@ -61,6 +61,7 @@ library(tidyverse)
 setwd("E:/chapter3/isochrons")
 isochrons = st_read("isochrons_snapped_qgis.gpkg")
 isochrons$ID = c(1:nrow(isochrons))
+targetcrs = st_crs(isochrons)
 # isochrons$area_check = st_area(isochrons)
 ## measurements are the same, do not apear to be cumulative- each polygon is measured separately; the variable "area" is in km
 isochrons$time = as.POSIXct(isochrons$time)
@@ -84,31 +85,45 @@ for(i in c(1:nrow(isochrons))){
   
   if(iso.temp$progtime != 0){
     pre.temp = touch.temp |> filter(time == iso.temp$lasttim)
-    pre.centroid = st_transform(st_centroid(pre.temp), crs = 4326)
-    
-    # tmap_mode("view")
-    # tm_shape(iso.temp) + tm_polygons() +
-    #   tm_shape(iso.centroid) + tm_dots() +
-    #   tm_shape(pre.temp) + tm_polygons(col = "blue") +
-    #   tm_shape(pre.centroid) + tm_dots()
-
-    if(nrow(pre.temp) == 1){
-      iso.temp$prepolyID = pre.temp$ID
-      iso.temp$fireline = sum(st_length(st_cast(st_intersection(iso.temp, pre.temp))))
-      iso.temp$spreaddir = as.numeric(mean((nngeo::st_azimuth(pre.centroid, iso.centroid))))
-      iso[[i]] = iso.temp
-    } else if(nrow(pre.temp) > 1){
-      iso.temp$prepolyID = list(pre.temp$ID)
-      iso.temp$fireline = sum(st_length(st_cast(st_intersection(iso.temp, pre.temp))))
-      iso.temp$spreaddir = as.numeric(mean((nngeo::st_azimuth(pre.centroid, iso.centroid))))
-      iso[[i]] = iso.temp
-    } else if(nrow(pre.temp) == 0){
+    if(nrow(pre.temp) == 0){
       print(paste0(i, " isochron does not overlap with any polygons with matching timestamps"))
       iso.temp$prepolyID = NA
       iso.temp$fireline = NA
       iso.temp$spreaddir = NA
       iso[[i]] = iso.temp
-    }
+    } else {
+      
+      pre.line = st_cast(st_intersection(iso.temp, pre.temp))
+      iso.temp$fireline = sum(st_length(pre.line))
+      pre.line = st_cast(pre.line, "POINT")
+      
+      if(nrow(pre.line) == 1){
+        pre.centroid = pre.line
+      } else if(nrow(pre.line) < 4){
+        pre.poly = rbind(pre.line, pre.line[nrow(pre.line):1,])
+
+        pre.centroid = pre.poly |> 
+          summarise(geom = st_combine(geom)) %>%
+          st_cast("POLYGON") |> 
+          st_centroid()
+        
+      } else if(nrow(pre.line) >= 4){
+        pre.centroid = pre.line |> 
+          summarise(geom = st_combine(geom)) %>%
+          st_cast("POLYGON") |> 
+          st_centroid()
+      }
+      
+      pre.centroid = st_transform(pre.centroid, crs = 4326)
+      iso.temp$spreaddir = as.numeric(mean((nngeo::st_azimuth(pre.centroid, iso.centroid))))
+      
+      if(nrow(pre.temp) == 1){
+        iso.temp$prepolyID = pre.temp$ID
+      } else if(nrow(pre.temp) > 1){
+        iso.temp$prepolyID = list(pre.temp$ID)
+      }
+      iso[[i]] = iso.temp
+    } 
   }
 }
 
@@ -123,21 +138,38 @@ nrow(iso2)
 # 121,431
 setwd("E:/chapter3/isochrons")
 st_write(iso2, "isochrons_withfireline.gpkg", delete_dsn = T)
-backup = iso2
+
+iso2 = st_read("isochrons_withfireline.gpkg")
+all(unique(iso2$ID) %in% unique(isochrons$ID))
+## FALSE
+length(unique(iso2$ID)) == length(unique(isochrons$ID))
+## FALSE
+
+missing = isochrons |> filter(!(ID %in% iso2$ID))
+all(missing$time == missing$lasttim)
+## TRUE
+## so all missing ID's from the original dataset correspond to first fire polygons
+
 st_geometry(iso2) = NULL
-iso2 = na.omit(iso2)
+iso2 = iso2 |> 
+  filter(!is.na(fireline) & progtime != 0)
+nrow(iso2)
+## 117,099
 cor(iso2 |> dplyr::select(area, fireline), method = "spearman")
-## 0.855
+## 0.85
 
 isochrons = backup
 isochrons$prog = isochrons$area/isochrons$progtime
 isochrons$spread = isochrons$area/isochrons$progtime/isochrons$fireline
 nrow(isochrons |> filter(is.infinite(spread)))
-## 1,023
+## 3,032
 ## may just have to remove these
 summary(isochrons |> filter(is.infinite(spread)))
+any(duplicated(isochrons$ID[is.infinite(isochrons$spread)]))
+## FALSE
 
-isochrons$fireline[isochrons$fireline == units::as_units(0, "m")] = units::as_units(1, "m")
+isochrons$fireline = as.numeric(isochrons$fireline)
+isochrons$fireline[isochrons$fireline == 0] = 1
 isochrons$spread = isochrons$area/isochrons$progtime/isochrons$fireline
 summary(isochrons)
 
@@ -145,4 +177,32 @@ isochrons = isochrons |>
   filter(progtime <= 24)
 nrow(isochrons)
 ## 70,370
-st_write(isochrons, "isochrons_withfireline_f1.gpkg", delete_dsn = T)
+# st_write(isochrons, "isochrons_withfireline_f1.gpkg", delete_dsn = T)
+st_write(isochrons, "isochrons_withfireline_withspreaddir.gpkg", delete_dsn = T)
+st_geometry(isochrons) = NULL
+isochrons = isochrons |> 
+  filter(!is.na(fireline))
+cor(isochrons |> dplyr::select(area, fireline, spread), method = "spearman")
+## 0.89, 0.65, 0.45
+
+iso3 = st_read("isochrons_gedi_m1.gpkg")
+backup2 = iso3
+st_geometry(iso3) = NULL
+iso3 = iso3 |> 
+  filter(!is.na(fireline))
+nrow(iso3)
+## 70,370
+cor(iso3 |> dplyr::select(area, fireline, spread), method = "spearman")
+## 0.89, 0.65, 0.45
+## looks the same
+
+nans = isochrons |> filter(is.na(spreaddir) & !is.na(spread))
+nrow(nans)
+## 2,925
+
+iso4 = st_read("isochrons_snapped_qgis.gpkg")
+iso4$ID = c(1:nrow(iso4))
+
+tm_shape(nans) + tm_polygons()
+tm_shape(nans[5,]) + tm_polygons(col = "red") + tm_shape(iso4 |> filter(ID == 135)) + tm_polygons()
+## all isochrons with NaN spreaddir is because the polygon is surrounded by the previous polygon- so the centerpoint for the fireline is actually inside the fire polygon
