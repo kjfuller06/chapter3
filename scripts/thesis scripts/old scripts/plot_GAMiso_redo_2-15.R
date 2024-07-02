d0d5b966b1a80947291d6ede07dff54900a3621f
kjfuller06 <kjfuller06@gmail.com>
2024-07-01 21:11:38 -0600

    tried to fix labels

--- scripts/thesis scripts/old scripts/plot_GAMiso_redo_2-15.R
+++ scripts/thesis scripts/old scripts/plot_GAMiso_redo_2-15.R
@@ -414,7 +414,7 @@ fit$x = fit$x/1000
 
 fit = fit |> 
   filter(x < max(rugdat.temp$predictor))
-g9 = summedallfun(dat = fit, rugdat = rugdat.temp, config = "xymin", lab2 = expression(atop("Distance to water (90"^th*" percentile; km)")), 
+g9 = summedallfun(dat = fit, rugdat = rugdat.temp, config = "xymin", lab2 = expression(atop("Distance to streams (orders 1-4; 90"^th*" percentile; km)")), 
                   # ymin = min(fit$ll), ymax = max(fit$ul),
                   ymin = ymin, ymax = ymax,
                   ab2 = max(fit$fit))
@@ -450,58 +450,3 @@ g5
 g4
 g1
 g3
-
-# maps ####
-setwd("D:/chapter1/bark-type-SDM/data")
-nsw = st_read("NSW_sans_islands.shp")
-nsw_buff = st_buffer(nsw, dist = 50000)
-aus = readRDS("gadm36_AUS_1_sp.rds")
-setwd("E:/chapter3/for GAMs")
-# g = st_read(paste0("ch3_forGAMs_poly_prefire", x, "_final.gpkg"))
-g = st_read(paste0("ch3_forGAMs_poly_prefire", x, "_final_redo_2-15.gpkg"))
-setwd("D:/chapter1/other_data/Final/terrain variables")
-# hill2 = raster("proj_dem_hillshade_30m.tif")
-# hill2 = aggregate(hill2, fact = 10)
-# hillproj = projectRaster(hill2, crs = "EPSG:4326")
-# hillproj = mask(hillproj, nsw)
-# writeRaster(hillproj, "hillshadeformapping.tif", overWrite = T)
-hill2 = raster("hillshadeformapping.tif")
-
-bb = st_buffer(g, dist = 50000)
-
-tmap_options(check.and.fix = T)
-tmap_mode("plot")
-t1 =
-  tm_shape(aus, bbox = bb) +
-  tm_fill(col = gray(75/100)) +
-  tm_shape(hill2) +
-  tm_raster(palette = gray(50:100 / 100), n = 100, legend.show = FALSE) +
-  # tm_shape(nsw) +
-  # tm_borders() +
-  tm_shape(g) + tm_fill(col = "prog", palette = turbo(20)[14:20], legend.show = FALSE) +
-  tm_graticules(lines = F) + 
-  tm_layout(inner.margins = 0)
-# t1
-
-setwd("C:/chapter3/outputs")
-# tmap_save(t1, "firepolygons.jpg", height = 3000, width = 2000, units = "px")
-tmap_save(t1, "firepolygons_redo_2-15.jpg", height = 3000, width = 2000, units = "px")
-
-t1 =
-  tm_shape(g) + tm_fill(col = "prog", palette = turbo(20)[14:20])
-t1
-
-setwd("C:/chapter3/outputs")
-tmap_save(t1, "polygons_legend.jpg", height = 3000, width = 2000, units = "px")
-
-# t1 =
-  tm_shape(aus, bbox = bb) +
-  tm_fill(col = gray(75/100)) +
-  tm_shape(hill2) +
-  tm_raster(palette = gray(50:100 / 100), n = 100, legend.show = FALSE) +
-  # tm_shape(nsw) +
-  # tm_borders() +
-  tm_shape(g) + tm_fill(col = "wind.stdev", palette = turbo(20)[14:20], legend.show = FALSE) +
-  tm_graticules(lines = F) + 
-  tm_layout(inner.margins = 0)
-# t1
